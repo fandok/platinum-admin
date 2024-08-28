33ece4f6d386f3ee9d2b9bdad6b8e8b830994dfa
Brian54309 <brianbudiarto@gmail.com>
2024-08-28 21:31:18 +0700

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import DashboardMain from "./pages/Dashboard";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <DashboardMain></DashboardMain>
+  </Provider>
 );
